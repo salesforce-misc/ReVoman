ac90de9c4ee02679b4b11674a38503a6f9ab0806
Gopal S Akshintala <gopalakshintala@gmail.com>
2022-04-25 23:20:06 +0530

    moment
    Signed-off-by: Gopal S Akshintala <gopalakshintala@gmail.com>

--- src/jvmTest/java/org/revcloud/ReVomanTest.java
+++ src/jvmTest/java/org/revcloud/ReVomanTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 class ReVomanTest {
   @Test
-  void lasso() {
+  void revUp() {
     final var itemToOutputType =
         Map.of(
             "All Pokemon", Results.class,
