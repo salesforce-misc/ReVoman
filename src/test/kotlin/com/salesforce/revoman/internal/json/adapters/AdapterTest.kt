6dc6aa99d7f12660148bbcdc58a8c51c1064e942
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2024-03-07 17:31:59 +0530

    Add AdapterTest
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

---
+++ src/test/kotlin/com/salesforce/revoman/internal/json/adapters/AdapterTest.kt
@@ -0,0 +1,62 @@
+/**
+ * ************************************************************************************************
+ * Copyright (c) 2023, Salesforce, Inc. All rights reserved. SPDX-License-Identifier: Apache License
+ * Version 2.0 For full license text, see the LICENSE file in the repo root or
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * ************************************************************************************************
+ */
+package com.salesforce.revoman.internal.json.adapters
+
+import com.salesforce.revoman.internal.json.initMoshi
+import com.salesforce.revoman.internal.json.moshiReVoman
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.adapter
+import io.kotest.matchers.shouldBe
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.*
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+class AdapterTest {
+  companion object {
+    @BeforeAll
+    @JvmStatic
+    fun setup() {
+      initMoshi()
+    }
+  }
+
+  @OptIn(ExperimentalStdlibApi::class)
+  @Test
+  fun `Epoch to Date`() {
+    val epochDate = 1604216172747
+    val jsonWithEpoch =
+      """
+      {
+        "date" : $epochDate
+      }
+    """
+        .trimIndent()
+    val result = moshiReVoman.adapter<BeanWithDate>().fromJson(jsonWithEpoch)
+    result?.date shouldBe Date.from(Instant.ofEpochSecond(epochDate))
+  }
+
+  @OptIn(ExperimentalStdlibApi::class)
+  @Test
+  fun `yyyy-mm-dd to Date`() {
+    val dateString = "2024-03-07"
+    val jsonWithEpoch =
+      """
+      {
+        "date" : "$dateString"
+      }
+    """
+        .trimIndent()
+    val result = moshiReVoman.adapter<BeanWithDate>().fromJson(jsonWithEpoch)
+    val formatter = SimpleDateFormat("yyyy-MM-dd")
+    result?.date shouldBe formatter.parse(dateString)
+  }
+
+  @JsonClass(generateAdapter = true) data class BeanWithDate(val date: Date)
+}
