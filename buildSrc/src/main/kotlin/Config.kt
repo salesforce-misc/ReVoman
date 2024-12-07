2bbd76888f51a0bab77740d89e1de3adb964d9d2
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2024-12-07 17:15:53 +0530

    Release 0.42.3 Upgraded Graal
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -6,6 +6,6 @@
  * ************************************************************************************************
  */
 const val GROUP_ID = "com.salesforce.revoman"
-const val VERSION = "0.42.2"
+const val VERSION = "0.42.3"
 const val ARTIFACT_ID = "revoman"
 const val STAGING_PROFILE_ID = "1ea0a23e61ba7d"
