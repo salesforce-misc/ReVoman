f9b8a885a571493e1345d28ddfb409198900e22b
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2026-03-14 08:23:06 +0530

    Use appropriate writer utils for RundownJsonWriter.kt
    Signed-off-by: Gopal S Akshintala <gopala.akshintala+sfemu@salesforce.com>

--- src/main/kotlin/com/salesforce/revoman/output/RundownJsonWriter.kt
+++ src/main/kotlin/com/salesforce/revoman/output/RundownJsonWriter.kt
@@ -29,134 +29,147 @@ fun Rundown.toJson(verbosity: Verbosity = Verbosity.STANDARD): String {
   val buffer = Buffer()
   JsonWriter.of(buffer).use { writer ->
     writer.indent = "  "
-    writer.beginObject()
-    writer.writeSummary(this)
-    writer.name("firstUnsuccessfulStepReport")
-    firstUnsuccessfulStepReport?.writeStepReportSummary(writer) ?: writer.nullValue()
-    if (verbosity >= Verbosity.STANDARD) {
-      listW("stepReports", stepReports, writer) { report ->
-        report.writeStepReport(writer, verbosity)
+    objW(this, writer) {
+      writeSummary(writer, this)
+      writer.name("firstUnsuccessfulStepReport")
+      firstUnsuccessfulStepReport?.writeStepReportSummary(writer) ?: writer.nullValue()
+      if (verbosity >= Verbosity.STANDARD) {
+        listW("stepReports", stepReports, writer) { report ->
+          report.writeStepReport(writer, verbosity)
+        }
+      }
+      if (verbosity == Verbosity.VERBOSE) {
+        writer.name("environment")
+        writeStringMap(writer, immutableEnv)
       }
     }
-    if (verbosity == Verbosity.VERBOSE) {
-      writer.name("environment")
-      writer.writeStringMap(immutableEnv)
-    }
-    writer.endObject()
   }
   return buffer.readUtf8()
 }
 
-private fun JsonWriter.writeSummary(rundown: Rundown) {
-  integer("providedStepsToExecuteCount", rundown.providedStepsToExecuteCount)
-  integer("executedStepCount", rundown.executedStepCount)
-  integer("httpFailureStepCount", rundown.httpFailureStepCount)
-  integer("unsuccessfulStepCount", rundown.unsuccessfulStepCount)
-  integer("executionFailureStepCount", rundown.executionFailureStepCount)
-  bool("areAllStepsSuccessful", rundown.areAllStepsSuccessful)
-  bool("areAllStepsExceptIgnoredSuccessful", rundown.areAllStepsExceptIgnoredSuccessful)
+private fun writeSummary(writer: JsonWriter, rundown: Rundown) {
+  integer("providedStepsToExecuteCount", rundown.providedStepsToExecuteCount, writer)
+  integer("executedStepCount", rundown.executedStepCount, writer)
+  integer("httpFailureStepCount", rundown.httpFailureStepCount, writer)
+  integer("unsuccessfulStepCount", rundown.unsuccessfulStepCount, writer)
+  integer("executionFailureStepCount", rundown.executionFailureStepCount, writer)
+  bool("areAllStepsSuccessful", rundown.areAllStepsSuccessful, writer)
+  bool("areAllStepsExceptIgnoredSuccessful", rundown.areAllStepsExceptIgnoredSuccessful, writer)
 }
 
 private fun StepReport.writeStepReportSummary(writer: JsonWriter) {
-  writer.beginObject()
-  writer.string("index", step.index)
-  writer.string("name", step.name)
-  writer.bool("isSuccessful", isSuccessful)
-  writer.writeFailure(this, Verbosity.SUMMARY)
-  writer.endObject()
+  objW(this, writer) {
+    string("index", step.index, writer)
+    string("name", step.name, writer)
+    bool("isSuccessful", isSuccessful, writer)
+    writeFailure(writer, this, Verbosity.SUMMARY)
+  }
 }
 
 private fun StepReport.writeStepReport(writer: JsonWriter, verbosity: Verbosity) {
-  writer.beginObject()
-  writer.string("index", step.index)
-  writer.string("name", step.name)
-  writer.string("displayName", step.displayName)
-  writer.bool("isSuccessful", isSuccessful)
-  writer.bool("isHttpStatusSuccessful", isHttpStatusSuccessful)
-  requestInfo?.fold({ it.requestInfo.httpMsg }, { it.httpMsg })?.let { request ->
-    writer.objW("request", request) {
-      writer.string("method", method.toString())
-      writer.string("uri", uri.toString())
-      if (verbosity == Verbosity.VERBOSE) {
-        writer.writeHeaders(this)
-        writer.string("body", bodyString())
-      }
+  objW(this, writer) {
+    string("index", step.index, writer)
+    string("name", step.name, writer)
+    string("displayName", step.displayName, writer)
+    bool("isSuccessful", isSuccessful, writer)
+    bool("isHttpStatusSuccessful", isHttpStatusSuccessful, writer)
+    writeRequestInfo(writer, verbosity)
+    writeResponseInfo(writer, verbosity)
+    writeFailure(writer, this, verbosity)
+    writePollingReport(writer, verbosity)
+    writePollingFailure(writer, verbosity)
+    if (verbosity == Verbosity.VERBOSE) {
+      writer.name("envSnapshot")
+      writeStringMap(writer, pmEnvSnapshot)
     }
   }
-  responseInfo?.fold({ it.responseInfo.httpMsg }, { it.httpMsg })?.let { response ->
-    writer.objW("response", response) {
-      writer.integer("statusCode", status.code)
-      writer.string("statusDescription", status.description)
-      if (verbosity == Verbosity.VERBOSE) {
-        writer.writeHeaders(this)
-        writer.string("body", bodyString())
-      }
-    }
-  }
-  writer.writeFailure(this, verbosity)
-  pollingReport?.let { pr ->
-    writer.objW("pollingReport", pr) {
-      writer.integer("pollAttempts", pollAttempts)
-      writer.lng("totalDurationMs", totalDuration.toMillis())
-      if (verbosity == Verbosity.VERBOSE) {
-        writer.integer("responseCount", responses.size)
-      }
-    }
-  }
-  pollingFailure?.let { pf ->
-    writer.objW("pollingFailure", pf) {
-      writer.string("message", failure.message)
-      when (this) {
-        is PollingTimeoutFailure -> {
-          writer.integer("pollAttempts", pollAttempts)
-          writer.lng("timeoutMs", timeout.toMillis())
-        }
-        is PollingRequestFailure -> {
-          writer.integer("pollAttempts", pollAttempts)
-        }
-      }
-      if (verbosity == Verbosity.VERBOSE) {
-        writer.string("stackTrace", failure.stackTraceToString())
-      }
-    }
-  }
-  if (verbosity == Verbosity.VERBOSE) {
-    writer.name("envSnapshot")
-    writer.writeStringMap(pmEnvSnapshot)
-  }
-  writer.endObject()
 }
 
-private fun JsonWriter.writeFailure(report: StepReport, verbosity: Verbosity) {
+private fun StepReport.writeRequestInfo(writer: JsonWriter, verbosity: Verbosity) {
+  requestInfo?.fold({ it.requestInfo.httpMsg }, { it.httpMsg })?.let {
+    objW("request", it, writer) { req ->
+      string("method", req.method.toString(), writer)
+      string("uri", req.uri.toString(), writer)
+      if (verbosity == Verbosity.VERBOSE) {
+        writeHeaders(writer, req)
+        string("body", req.bodyString(), writer)
+      }
+    }
+  }
+}
+
+private fun StepReport.writeResponseInfo(writer: JsonWriter, verbosity: Verbosity) {
+  responseInfo?.fold({ it.responseInfo.httpMsg }, { it.httpMsg })?.let {
+    objW("response", it, writer) { res ->
+      integer("statusCode", res.status.code, writer)
+      string("statusDescription", res.status.description, writer)
+      if (verbosity == Verbosity.VERBOSE) {
+        writeHeaders(writer, res)
+        string("body", res.bodyString(), writer)
+      }
+    }
+  }
+}
+
+private fun StepReport.writePollingReport(writer: JsonWriter, verbosity: Verbosity) {
+  pollingReport?.let {
+    objW("pollingReport", it, writer) { pr ->
+      integer("pollAttempts", pr.pollAttempts, writer)
+      lng("totalDurationMs", pr.totalDuration.toMillis(), writer)
+      if (verbosity == Verbosity.VERBOSE) {
+        integer("responseCount", pr.responses.size, writer)
+      }
+    }
+  }
+}
+
+private fun StepReport.writePollingFailure(writer: JsonWriter, verbosity: Verbosity) {
+  pollingFailure?.let {
+    objW("pollingFailure", it, writer) { pf ->
+      string("message", pf.failure.message, writer)
+      when (pf) {
+        is PollingTimeoutFailure -> {
+          integer("pollAttempts", pf.pollAttempts, writer)
+          lng("timeoutMs", pf.timeout.toMillis(), writer)
+        }
+        is PollingRequestFailure -> {
+          integer("pollAttempts", pf.pollAttempts, writer)
+        }
+      }
+      if (verbosity == Verbosity.VERBOSE) {
+        string("stackTrace", pf.failure.stackTraceToString(), writer)
+      }
+    }
+  }
+}
+
+private fun writeFailure(writer: JsonWriter, report: StepReport, verbosity: Verbosity) {
   report.failure?.fold(
     { exeFailure: ExeFailure ->
-      objW("failure", exeFailure) {
-        string("type", exeType.toString())
-        string("message", failure.message)
+      objW("failure", exeFailure, writer) { ef ->
+        string("type", ef.exeType.toString(), writer)
+        string("message", ef.failure.message, writer)
         if (verbosity == Verbosity.VERBOSE) {
-          string("stackTrace", failure.stackTraceToString())
+          string("stackTrace", ef.failure.stackTraceToString(), writer)
         }
       }
     },
     { httpUnsuccessful: HttpStatusUnsuccessful ->
-      objW("failure", httpUnsuccessful) {
-        string("type", exeType.toString())
-        integer("httpStatusCode", responseInfo.httpMsg.status.code)
-        string("httpStatusDescription", responseInfo.httpMsg.status.description)
+      objW("failure", httpUnsuccessful, writer) { hu ->
+        string("type", hu.exeType.toString(), writer)
+        integer("httpStatusCode", hu.responseInfo.httpMsg.status.code, writer)
+        string("httpStatusDescription", hu.responseInfo.httpMsg.status.description, writer)
       }
     },
   )
 }
 
-private fun JsonWriter.writeHeaders(httpMsg: HttpMessage) {
-  name("headers")
-  beginObject()
-  httpMsg.headers.forEach { (key, value) -> string(key, value) }
-  endObject()
+private fun writeHeaders(writer: JsonWriter, httpMsg: HttpMessage) {
+  objW("headers", httpMsg, writer) { msg ->
+    msg.headers.forEach { (key, value) -> string(key, value, writer) }
+  }
 }
 
-private fun JsonWriter.writeStringMap(map: Map<String, Any?>) {
-  beginObject()
-  map.forEach { (key, value) -> string(key, value?.toString()) }
-  endObject()
+private fun writeStringMap(writer: JsonWriter, map: Map<String, Any?>) {
+  objW(map, writer) { m -> m.forEach { (key, value) -> string(key, value?.toString(), writer) } }
 }
