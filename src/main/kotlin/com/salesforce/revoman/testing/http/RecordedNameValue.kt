452ad046c98bab97a8b0d6926cfaae14b7f3d151
Gopal S Akshintala <gopalakshintala@gmail.com>
2026-08-15 10:07:01 +0530

    feat: add mock HTTP request contracts

---
+++ src/main/kotlin/com/salesforce/revoman/testing/http/RecordedNameValue.kt
@@ -0,0 +1,11 @@
+/**
+ * ************************************************************************************************
+ * Copyright (c) 2026, Salesforce, Inc. All rights reserved. SPDX-License-Identifier: Apache License
+ * Version 2.0 For full license text, see the LICENSE file in the repo root or
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * ************************************************************************************************
+ */
+package com.salesforce.revoman.testing.http
+
+/** A recorded name/value pair from an HTTP request. */
+@JvmRecord data class RecordedNameValue(val name: String, val value: String?)
